7fad82e427a2f7aac37718f0bfe0b6e577414b78
tashian <carl@tashian.com>
2018-05-11 14:41:06 -0700

    Test code and FxQueue changes
    - Get test.js working again
    - Remove width restrictions on FxQueue (which wasn't working anyway due to bug)
    - Fix bug with width() function in FxQueue

--- test.js
+++ test.js
@@ -1,8 +1,46 @@
 var requirejs = require('requirejs');
 var expect = require('chai').expect;
-var common = requirejs('./lib/common.js');
+$ = { level: 0 };
 
-var s = common.sigmoid(10, 20, 8);
-console.log(s);
-expect(s).to.equal([10, 13, 15, 17, 20]);
+requirejs.config({
+	paths: {
+		requireLib: "node_modules/requirejs/require",
+		underscore: "node_modules/underscore/underscore",
+        utils: "lib/utils",
+        machine: "lib/machine",
+        polling_timer: "lib/polling_timer",
+        fx_queue: "lib/fx_queue"
+	},
+    baseUrl: __dirname
+});
 
+requirejs([
+    "underscore",
+    "utils",
+	"fx_queue",
+    "polling_timer",
+    "machine"
+],
+function(
+    _,
+    utils,
+    FxQueue,
+    PollingTimer,
+    Machine
+) {
+
+	var x = new FxQueue();
+
+	// console.log(utils.ease(80, 90));
+	// console.log(utils.ease(20, 30));
+	// console.log(utils.ease(0, 0));
+	// console.log(utils.ease(3, 0));
+
+	console.log(x.width());
+	x.add(1, 'fadeOut', 50);
+	x.add(2, 'fadeOut', 50);
+	x.add(3, 'fadeOut', 50);
+	console.log(x.width());
+	console.log(x.size());
+
+});
